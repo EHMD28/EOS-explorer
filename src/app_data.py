b602f8252337906a4c088f8f2fe4830fdf8b3a81
EHMD28 <ehmdiagne0530@gmail.com>
2026-03-13 08:52:40 -0400

    random streamlit testing

---
+++ src/app_data.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x_values = np.linspace(0, 2 * np.pi, 100)
+sin_y_values = np.sin(x_values)
+cos_y_values = np.cos(x_values)
+fig, ax = plt.subplots()
+
+ax.plot(x_values, sin_y_values, color="red", label="sin x")
+ax.plot(x_values, cos_y_values, color="blue", label="cos x")
+ax.axhline(y=0, linestyle="--")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+fig.legend()
+fig.tight_layout()
--- src/main.py
+++ src/main.py
@@ -4,21 +4,8 @@ Here's our first attempt at using data to create a table:
 """
 
 import streamlit as st
-import numpy as np
-import matplotlib.pyplot as plt
+from app_data import fig
 
-x_values = np.linspace(0, 2 * np.pi, 100)
-sin_y_values = np.sin(x_values)
-cos_y_values = np.cos(x_values)
-fig, ax = plt.subplots()
-
-ax.plot(x_values, sin_y_values, color="red")
-ax.plot(x_values, cos_y_values, color="blue")
-ax.axhline(y=0, linestyle="--")
-ax.set_xlabel("x")
-ax.set_ylabel("y")
-fig.tight_layout()
-
-st.markdown(":red[f(x) = sin x]")
-st.markdown(":blue[f(x) = cos x]")
+st.latex(r"f(x) = \sin(x)")
+st.latex(r"g(x) = \cos(x)")
 st.pyplot(fig)
