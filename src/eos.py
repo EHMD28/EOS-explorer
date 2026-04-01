8eef6944a439bf14fca1cde0dafd9a7452b64f40
EHMD28 <ehmdiagne0530@gmail.com>
2026-04-01 10:48:06 -0400

    added static dimensionless data

--- src/eos.py
+++ src/eos.py
@@ -39,9 +39,17 @@ def eos_eps(p: float) -> float:
     return eps
 
 
-def eos_eps_prime(p: float):
+def get_dimensionless_data():
     densities, pressures = EOS_DATA
     densities = [eps * ScalingConstants.MEV_PER_FM3_TO_SM_PER_KM_3 for eps in densities]
     densities, pressures = (eps_prime(densities), p_prime(pressures))
+    return (densities, pressures)
+
+
+DIMENSIONLESS_DATA = get_dimensionless_data()
+
+
+def eos_eps_prime(p: float):
+    pressures, densities = DIMENSIONLESS_DATA
     eps = np.interp(p, pressures, densities)
     return eps
