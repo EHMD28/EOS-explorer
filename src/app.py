d36d18f049b668190e8bf66c53676b2a2ac4dea4
EHMD28 <ehmdiagne0530@gmail.com>
2026-04-30 17:56:16 -0400

    changed color cycler

--- src/app.py
+++ src/app.py
@@ -2,20 +2,36 @@
 This file in the entry point of the application.
 """
 
+from cycler import cycler
+import matplotlib
 import streamlit as st
 
 from app_constants import AppConstants
 from ui.all_ui import draw_ui
 
 
-def configure_streamlit():
+def configure_app():
     st.set_page_config(
         page_title="EoS Explorer", page_icon=AppConstants.FAVICON_PATH, layout="wide"
+    )
+    # Configure the default color cycle for Matplotlib so the colors don't
+    # accidentally overlap.
+    matplotlib.rcParams["axes.prop_cycle"] = cycler(
+        color=[
+            "orange",  # I think orange constrasts best with blue.
+            "red",
+            "green",
+            "indigo",
+            "violet",
+            "gray",
+            "brown",
+            "black",
+        ]
     )
 
 
 def main():
-    configure_streamlit()
+    configure_app()
     draw_ui()
 
 
