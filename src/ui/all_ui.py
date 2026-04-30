c65ae9c7281c6cd79b18bee6c0181dfa80eabd8f
EHMD28 <ehmdiagne0530@gmail.com>
2026-04-30 18:34:01 -0400

    removed debug tab

--- src/ui/all_ui.py
+++ src/ui/all_ui.py
@@ -1,8 +1,6 @@
-from ui.debugging_tab import draw_ui_for_dimensionless_conversion
 from ui.eos_tab import (
     draw_and_get_eos_dropdown,
     draw_ui_for_polytropic_eos,
-    draw_ui_for_soc_eos,
     draw_ui_for_tabulated_eos,
 )
 
@@ -13,8 +11,8 @@ def draw_ui():
     """
     Write components to user interface.
     """
-    tab_names = ["Main", "Debugging"]
-    main_tab, debug_tab = st.tabs(tab_names)
+    tab_names = ["Main", "Info"]
+    main_tab, info_tab = st.tabs(tab_names)
     with main_tab:
         eos_dropdown = draw_and_get_eos_dropdown()
         match eos_dropdown:
@@ -22,7 +20,5 @@ def draw_ui():
                 draw_ui_for_polytropic_eos()
             case "Tabulated":
                 draw_ui_for_tabulated_eos()
-            case "Speed-of-Sound Interpolation":
-                draw_ui_for_soc_eos()
-    with debug_tab:
-        draw_ui_for_dimensionless_conversion()
+    with info_tab:
+        ...
